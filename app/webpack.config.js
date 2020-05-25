8d593d46633ff96795981465d842aba54f0c2a47
Emzawadzki <emzawadzki@gmail.com>
2020-05-25 20:52:39 +0200

    Format files

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,29 +1,29 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.tsx",
-    devServer: {
-        inline: true,
-        port: 8000,
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    output: {
-        filename: "app.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                loader: "ts-loader",
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-        }),
+  entry: "./src/index.tsx",
+  devServer: {
+    inline: true,
+    port: 8000,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
