3b608a6f8fd8fd06a1439f963d1b7ac66786b98f
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-05-27 15:39:13 +0200

    added User class

---
+++ src/main/java/mateusz/michal/chat/Model/User.java
@@ -0,0 +1,22 @@
+package mateusz.michal.chat.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
