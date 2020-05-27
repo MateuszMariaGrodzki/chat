89fe357095e9281de1b5a44c3d20b976a4558889
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-05-27 16:20:16 +0200

    added @Data adnotation to User and Message classes

--- src/main/java/mateusz/michal/chat/Model/User.java
+++ src/main/java/mateusz/michal/chat/Model/User.java
@@ -1,10 +1,13 @@
 package mateusz.michal.chat.Model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "user")
+@Data
 public class User {
 
     @Id
