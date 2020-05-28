3b9a18b2a089f707cba778b4817b2a3d054dfebd
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-05-28 15:47:04 +0200

    added UserRepositorty interface

---
+++ src/main/java/mateusz/michal/chat/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package mateusz.michal.chat.Repository;
+
+import mateusz.michal.chat.Model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User , Integer> {
+    User findByEmail(String email);
+    User findByName(String name);
+}
