45176b7746050e38625cc3dcdbc643096c23605a
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-05-29 16:37:49 +0200

    added UserService class

---
+++ src/main/java/mateusz/michal/chat/Service/UserService.java
@@ -0,0 +1,42 @@
+package mateusz.michal.chat.Service;
+
+import mateusz.michal.chat.Model.Role;
+import mateusz.michal.chat.Model.User;
+import mateusz.michal.chat.Repository.RoleRepository;
+import mateusz.michal.chat.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmal(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public User findByName(String name){
+        return userRepository.findByName(name);
+    }
+
+    public User saveUser(User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+}
