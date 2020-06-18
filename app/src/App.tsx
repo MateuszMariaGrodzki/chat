6622d3ec02f1de284e577cdc8b54e3393cd7ddef
Emzawadzki <emzawadzki@gmail.com>
2020-06-18 21:14:13 +0200

    Create basic routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Register from "./Register";
+import Home from "./Home";
 
 const App = () => (
-  <div data-testid="app">
-    <h1>Hello Chat app!</h1>
-  </div>
+  <Router>
+    <Header />
+    <Switch>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
---
+++ app/src/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </header>
+  );
+};
+
+export default Header;
