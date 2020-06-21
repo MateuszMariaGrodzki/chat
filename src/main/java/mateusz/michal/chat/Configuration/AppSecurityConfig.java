a6066fbd36c2ce9c41c1c00b84645fb13e0b5b20
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-06-21 16:19:45 +0200

    ovverride configure method in AppSecurtyConfig class

--- src/main/java/mateusz/michal/chat/Configuration/AppSecurityConfig.java
+++ src/main/java/mateusz/michal/chat/Configuration/AppSecurityConfig.java
@@ -1,13 +1,30 @@
 package mateusz.michal.chat.Configuration;
 
+import mateusz.michal.chat.Service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
