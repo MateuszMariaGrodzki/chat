7700b834d582be0f7b8e4a6cdda629eabd4bc376
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-03 13:58:54 +0200

    added simple validation in Controller

--- src/main/java/mateusz/michal/chat/Controller/RegistrationController.java
+++ src/main/java/mateusz/michal/chat/Controller/RegistrationController.java
@@ -1,9 +1,12 @@
 package mateusz.michal.chat.Controller;
 
 import mateusz.michal.chat.Model.User;
+import mateusz.michal.chat.Service.RegistrationService;
 import mateusz.michal.chat.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
@@ -11,10 +14,15 @@ import org.springframework.web.bind.annotation.*;
 public class RegistrationController {
 
     @Autowired
-    UserService userService;
+    RegistrationService registrationService;
 
     @PostMapping(value = "/register",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createNewUser(@RequestBody User user){
-        userService.saveUser(user);
+    public ResponseEntity<String> createNewUser(@RequestBody User user){
+        try {
+            registrationService.saveUserToDatabase(user);
+            return ResponseEntity.ok("Registered");
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
