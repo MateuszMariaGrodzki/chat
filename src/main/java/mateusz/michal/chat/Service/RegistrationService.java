c9295bb29de27f6228dc0beed526dd0621c3a542
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-03 13:28:06 +0200

    added saveUserToDatabase method in RegisterService class

--- src/main/java/mateusz/michal/chat/Service/RegistrationService.java
+++ src/main/java/mateusz/michal/chat/Service/RegistrationService.java
@@ -1,8 +1,10 @@
 package mateusz.michal.chat.Service;
 
+import com.sun.istack.NotNull;
 import mateusz.michal.chat.Model.Role;
 import mateusz.michal.chat.Model.User;
 import mateusz.michal.chat.Repository.RoleRepository;
+import mateusz.michal.chat.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,10 +21,32 @@ public class RegistrationService {
     @Autowired
     RoleRepository roleRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     private User createUser(User user){
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         Role userRole = roleRepository.findByRole("USER");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         return user;
+    }
+
+    private boolean isUserNotInDatabaseByName(String name){
+        User user = userRepository.findByName(name);
+        return user == null;
+    }
+
+    private boolean isUserNotInDatabaseByEmail(String email){
+        User user = userRepository.findByEmail(email);
+        return user == null;
+    }
+
+    public void saveUserToDatabase(@NotNull User user) throws Exception {
+        if(isUserNotInDatabaseByEmail(user.getName()) || isUserNotInDatabaseByEmail(user.getEmail())) {
+            User userToSave = createUser(user);
+            userRepository.save(userToSave);
+        } else {
+            throw new Exception("Podane imię lub email jest już zajęte");
+        }
     }
 }
