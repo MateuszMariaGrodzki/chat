05bc0431f20b29b5b6c139716a9277743b234c97
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-06 15:56:15 +0200

    Mocked dependencies

--- src/test/java/mateusz/michal/chat/Service/RegistrationServiceTest.java
+++ src/test/java/mateusz/michal/chat/Service/RegistrationServiceTest.java
@@ -1,4 +1,31 @@
 package mateusz.michal.chat.Service;
 
+import mateusz.michal.chat.Repository.RoleRepository;
+import mateusz.michal.chat.Repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class RegistrationServiceTest {
+
+    @InjectMocks
+    RegistrationService registrationService;
+
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Mock
+    RoleRepository roleRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 }
