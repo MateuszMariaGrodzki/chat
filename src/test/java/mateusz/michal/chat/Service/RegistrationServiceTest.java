249d4029caafd2e7a30535f74b9cf42cdb06c9c2
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-06 17:32:52 +0200

    added test for name_missing error code

--- src/test/java/mateusz/michal/chat/Service/RegistrationServiceTest.java
+++ src/test/java/mateusz/michal/chat/Service/RegistrationServiceTest.java
@@ -1,31 +1,34 @@
 package mateusz.michal.chat.Service;
 
-import mateusz.michal.chat.Repository.RoleRepository;
-import mateusz.michal.chat.Repository.UserRepository;
+import mateusz.michal.chat.Model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+
 
 public class RegistrationServiceTest {
 
     @InjectMocks
     RegistrationService registrationService;
 
-    @Mock
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Mock
-    RoleRepository roleRepository;
-
-    @Mock
-    UserRepository userRepository;
-
     @BeforeEach
     void setUp(){
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Test saveUserToDatabase with invalid name")
+    void shouldNameBeMissing() throws Exception{
+        User user = new User();
+        user.setName("");
+        user.setPassword("AlamaKota3@");
+        user.setEmail("Ala325@gmail.com");
+        assertEquals("name_missing",registrationService.saveUserToDatabase(user));
     }
 }
