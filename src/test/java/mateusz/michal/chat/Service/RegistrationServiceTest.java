cbefe77878514c4ccd83e06af4c51914b3199d69
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-07 17:12:37 +0200

    added test for name_occupied error code

--- src/test/java/mateusz/michal/chat/Service/RegistrationServiceTest.java
+++ src/test/java/mateusz/michal/chat/Service/RegistrationServiceTest.java
@@ -1,6 +1,7 @@
 package mateusz.michal.chat.Service;
 
 import mateusz.michal.chat.Model.User;
+import mateusz.michal.chat.Repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,13 +10,18 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 
 public class RegistrationServiceTest {
 
     @InjectMocks
     RegistrationService registrationService;
+
+    @Mock
+    UserRepository userRepository;
 
     @BeforeEach
     void setUp(){
@@ -52,4 +58,18 @@ public class RegistrationServiceTest {
         assertEquals("password_missing", registrationService.saveUserToDatabase(user));
     }
 
+    @Test
+    @DisplayName("Test for saveUserToDatabase with name occupied")
+    void shouldNameBeInDatabase(){
+        User user = new User();
+        user.setName("Mateusz");
+        user.setEmail("mateusz@gmail.com");
+        user.setPassword("AlaMaKota3@");
+        User userFromDatabase = new User();
+        user.setName("Mateusz");
+        user.setEmail("mati402@onet.pl");
+        user.setPassword("ala");
+        when(userRepository.findByName(anyString())).thenReturn(userFromDatabase);
+        assertEquals("name_occupied", registrationService.saveUserToDatabase(user));
+    }
 }
