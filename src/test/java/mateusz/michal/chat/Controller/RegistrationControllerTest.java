8b884e0f10728e9bc1cbfa06cf358041c0d26be4
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-10 17:22:38 +0200

    added test response for name missing error code

--- src/test/java/mateusz/michal/chat/Controller/RegistrationControllerTest.java
+++ src/test/java/mateusz/michal/chat/Controller/RegistrationControllerTest.java
@@ -1,7 +1,9 @@
 package mateusz.michal.chat.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import mateusz.michal.chat.Model.JsonRespond;
 import mateusz.michal.chat.Model.User;
 import mateusz.michal.chat.Service.MyUserDetailsService;
 import mateusz.michal.chat.Service.RegistrationService;
@@ -13,9 +15,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = RegistrationController.class)
@@ -37,9 +42,25 @@ public class RegistrationControllerTest {
     @DisplayName("Test for parsing JSON in /apr/register endpoint")
     void JSONShouldBeParsedToJavaObject() throws Exception {
         User user = User.builder().name("Mateusz").email("saka@dsa.com").password("kotlkas@qs3").build();
-        mockMvc.perform(post("/api/register", 42L)
+        mockMvc.perform(post("/api/register")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test response with name_missing error code")
+    void testResponseWithNameMissing() throws Exception {
+        User user = User.builder().name("").email("saka@dsa.com").password("").build();
+        when(registrationService.saveUserToDatabase(user)).thenReturn("name_missing");
+        MvcResult mvcResult = mockMvc.perform(post("/api/register")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andReturn();
+        JsonRespond error = new JsonRespond("name_missing");
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+        assertThat(actualResponseBody).isEqualTo(
+                objectMapper.writeValueAsString(error));
     }
 }
