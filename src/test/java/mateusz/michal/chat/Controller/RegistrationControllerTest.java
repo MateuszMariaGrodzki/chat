ee376a9ff1ea9df46135c010e025be1cd7487708
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-10 12:47:16 +0200

    added RegistrationControllerTest class

---
+++ src/test/java/mateusz/michal/chat/Controller/RegistrationControllerTest.java
@@ -0,0 +1,15 @@
+package mateusz.michal.chat.Controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = RegistrationController.class)
+public class RegistrationControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+}
