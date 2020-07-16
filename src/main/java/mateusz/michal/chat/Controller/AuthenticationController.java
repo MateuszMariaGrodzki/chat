1a56f69fff72a2ba086083baa7fb6e0259e74291
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-16 16:13:33 +0200

    added AuthenticationController class with createAuthenticationToken method

---
+++ src/main/java/mateusz/michal/chat/Controller/AuthenticationController.java
@@ -0,0 +1,16 @@
+package mateusz.michal.chat.Controller;
+
+import mateusz.michal.chat.Model.JwtTokenRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class AuthenticationController {
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> createAuthenticaionToken(@RequestBody JwtTokenRequest jwtTokenRequest){
+        return ResponseEntity.ok(jwtTokenRequest.getUsername());
+    }
+}
