cbe2d595ccc1c762351826f8ac79010613cbb62c
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-16 16:01:01 +0200

    added class needed for retrieving data from login form

---
+++ src/main/java/mateusz/michal/chat/Model/JwtTokenRequest.java
@@ -0,0 +1,13 @@
+package mateusz.michal.chat.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtTokenRequest {
+    private String username;
+    private String password;
+}
