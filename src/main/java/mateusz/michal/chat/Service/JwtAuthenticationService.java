e7c857493d4163e9d3c9e56e7dc2773acee6b576
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-17 12:51:24 +0200

    added method which provide UserDetails from data recieved in login form

--- src/main/java/mateusz/michal/chat/Service/JwtAuthenticationService.java
+++ src/main/java/mateusz/michal/chat/Service/JwtAuthenticationService.java
@@ -2,6 +2,9 @@ package mateusz.michal.chat.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +12,10 @@ public class JwtAuthenticationService {
 
     @Autowired
     AuthenticationManager authenticationManager;
+
+    private UserDetails provideUserDetailsFromLoginForm(String username, String password){
+        Authentication authentication = authenticationManager.
+                authenticate(new UsernamePasswordAuthenticationToken(username,password));
+        return (UserDetails) authentication.getPrincipal();
+    }
 }
