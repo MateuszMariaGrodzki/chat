343904359223645e76f80591848f0987d1fdde53
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-23 17:50:54 +0200

    added generateToken and authenticate methods to JweAuthenticationService class

--- src/main/java/mateusz/michal/chat/Service/JwtAuthenticationService.java
+++ src/main/java/mateusz/michal/chat/Service/JwtAuthenticationService.java
@@ -1,17 +1,43 @@
 package mateusz.michal.chat.Service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import mateusz.michal.chat.Model.JwtTokenRequest;
+import mateusz.michal.chat.Model.JwtTokenResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class JwtAuthenticationService {
 
     @Autowired
     AuthenticationManager authenticationManager;
+
+    @Value("${SECRETKEY}")
+    private String SECRET_KEY;
+
+    public JwtTokenResponse authenticate(JwtTokenRequest jwtTokenRequest){
+        UserDetails userDetails = provideUserDetailsFromLoginForm(jwtTokenRequest.getUsername(),
+                jwtTokenRequest.getPassword());
+        String token = generateToken(userDetails.getUsername());
+        return new JwtTokenResponse(token);
+    }
+
+    private String generateToken(String username){
+        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+        String token = JWT.create()
+                .withSubject(username)
+                .withIssuedAt(new Date())
+                .sign(algorithm);
+        return token;
+    }
 
     private UserDetails provideUserDetailsFromLoginForm(String username, String password){
         Authentication authentication = authenticationManager.
