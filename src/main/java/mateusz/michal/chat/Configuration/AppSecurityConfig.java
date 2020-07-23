6247626baf4b0921edbd104c2ff5e3cb9df72ae4
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-23 17:46:39 +0200

    added AuthenticationManager Bean to AppConfiguration

--- src/main/java/mateusz/michal/chat/Configuration/AppSecurityConfig.java
+++ src/main/java/mateusz/michal/chat/Configuration/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import mateusz.michal.chat.Service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService)
                 .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
     }
 
     @Override
