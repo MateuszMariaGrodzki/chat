08f831186f0bb4388493cdf65a73601ae2f6d5d5
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-27 12:32:50 +0200

    removed unused methods

--- src/main/java/mateusz/michal/chat/Service/MyUserDetailsService.java
+++ src/main/java/mateusz/michal/chat/Service/MyUserDetailsService.java
@@ -1,6 +1,5 @@
 package mateusz.michal.chat.Service;
 
-import mateusz.michal.chat.Model.Role;
 import mateusz.michal.chat.Model.User;
 import mateusz.michal.chat.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,29 +12,13 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
-
-    private List<GrantedAuthority> getUserAuthorities(Set<Role> userRoles){
-        Set<GrantedAuthority> roles = new HashSet<>();
-        for(Role role : userRoles){
-            roles.add(new SimpleGrantedAuthority(role.getRole()));
-        }
-        List<GrantedAuthority> authorities = new ArrayList<>(roles);
-        return authorities;
-    }
-
-    private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities){
-        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(),
-                true, true, true, true, authorities);
-    }
 
     @Override
     @Transactional
