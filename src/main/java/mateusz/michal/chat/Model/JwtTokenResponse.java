59459e606034be8c41d785c199e72d02052fe995
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-07-29 12:07:07 +0200

    added errorCode property to JwtTokenResponse model

--- src/main/java/mateusz/michal/chat/Model/JwtTokenResponse.java
+++ src/main/java/mateusz/michal/chat/Model/JwtTokenResponse.java
@@ -2,13 +2,19 @@ package mateusz.michal.chat.Model;
 
 public class JwtTokenResponse {
 
+    String errorCode;
     String token;
 
-    public JwtTokenResponse(String token) {
+    public JwtTokenResponse(String errorCode, String token) {
+        this.errorCode = errorCode;
         this.token = token;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public String getErrorCode(){
+        return errorCode;
     }
 }
