8bab39d2b249b85d3808abbc7fdc5ed8a9396ee9
Emzawadzki <emzawadzki@gmail.com>
2020-07-30 01:19:18 +0200

    Add register errors typing

--- app/src/api/types.ts
+++ app/src/api/types.ts
@@ -4,6 +4,16 @@ type ErrorResponse = {
 
 export type POSTRequest = <T extends keyof POST>(url: T, data: POST[T]["request"]) => Promise<POST[T]["response"] | ErrorResponse>;
 
+export enum REGISTER_ERROR {
+  name_missing = "name_missing",
+  email_missing = "email_missing",
+  password_missing = "password_missing",
+  name_occupied = "name_occupied",
+  email_occupied = "email_occupied",
+  email_incorrect = "email_incorrect",
+  weak_password = "weak_password",
+}
+
 export interface POST {
   register: {
     request: {
@@ -12,7 +22,7 @@ export interface POST {
       password: string;
     };
     response: {
-      errorCode: string;
+      errorCode: REGISTER_ERROR;
       registered: "true" | "false";
     };
   };
