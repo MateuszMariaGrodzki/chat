4477ce1020d3b396dffb5bf5901ec6d40a008b6a
Emzawadzki <emzawadzki@gmail.com>
2020-12-19 21:19:50 +0100

    Move RegisterForm to separate component

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,59 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
-import { Button, Box, Snackbar } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
 
-import API from "../api";
-import { PageTitle, Input, Form } from "../common";
-import { RegisterError } from "../api/types";
-import { registerFeedback } from "../api/errorMaps";
-import { useInput, useStatus } from "../hooks";
+import { PageTitle } from "../common";
+import RegisterForm from "../components/RegisterForm";
 
 const Register = () => {
-  const [name, handleUsernameChange] = useInput();
-  const [email, handleEmailChange] = useInput();
-  const [password, handlePasswordChange] = useInput();
-  const [status, setStatus] = useStatus();
-  const [error, setError] = useState<Maybe<RegisterError>>(null);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    setStatus("pending");
-    e.preventDefault();
-    const { errorCode, ...data } = await API.register({ name, email, password });
-    if (errorCode !== null) {
-      setStatus("error");
-      setError(errorCode);
-    } else {
-      setStatus("success");
-    }
-  };
-
-  const handleSnackbarClose = () => {
-    setStatus(null);
-  };
-
   return (
     <>
       <Helmet>
         <title>Chat App - Register</title>
       </Helmet>
       <PageTitle>Join the best chatters!</PageTitle>
-      <Form onSubmit={handleSubmit}>
-        <Input placeholder="Username" type="text" name="name" id="username" onChange={handleUsernameChange} value={name} fullWidth />
-        <Input placeholder="E-mail address" type="email" name="email" id="email" onChange={handleEmailChange} value={email} fullWidth />
-        <Input placeholder="Password" type="password" name="pwd" id="pwd" onChange={handlePasswordChange} value={password} fullWidth />
-        <Box mt={2} display="flex" justifyContent="center">
-          <Button variant="contained" size="large" type="submit" color="primary" disabled={status === "pending"}>
-            Submit
-          </Button>
-        </Box>
-      </Form>
-      <Snackbar open={status === "success"} autoHideDuration={3000} onClose={handleSnackbarClose}>
-        <Alert severity="success">Registered successfully!</Alert>
-      </Snackbar>
-      <Snackbar open={status === "error"} autoHideDuration={3000} onClose={handleSnackbarClose}>
-        <Alert severity="error">{registerFeedback[error]}</Alert>
-      </Snackbar>
+      <RegisterForm />
     </>
   );
 };
