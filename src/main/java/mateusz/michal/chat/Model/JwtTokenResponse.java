3fd9568ccdd971ed4f5d585934d49680d1adefbd
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-12-19 15:43:15 +0100

    added name and email property to JwtTokenResponse model and update constructor

--- src/main/java/mateusz/michal/chat/Model/JwtTokenResponse.java
+++ src/main/java/mateusz/michal/chat/Model/JwtTokenResponse.java
@@ -3,10 +3,14 @@ package mateusz.michal.chat.Model;
 public class JwtTokenResponse {
     JwtAuthenticationErrorCode errorCode;
     String token;
+    String name;
+    String email;
 
-    public JwtTokenResponse(JwtAuthenticationErrorCode errorCode, String token) {
+    public JwtTokenResponse(JwtAuthenticationErrorCode errorCode, String token, String name, String email) {
         this.errorCode = errorCode;
         this.token = token;
+        this.name = name;
+        this.email = email;
     }
 
     public String getToken() {
