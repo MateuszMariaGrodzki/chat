3cad5d52dc2e000a5b18aa09868ff08be1487e87
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-12-21 16:06:11 +0100

    changed class name

--- src/main/java/mateusz/michal/chat/Controller/AuthenticationController.java
+++ src/main/java/mateusz/michal/chat/Controller/AuthenticationController.java
@@ -2,7 +2,7 @@ package mateusz.michal.chat.Controller;
 
 import mateusz.michal.chat.Model.JwtTokenRequest;
 import mateusz.michal.chat.Model.JwtTokenResponse;
-import mateusz.michal.chat.Service.JwtAuthenticationService;
+import mateusz.michal.chat.Service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
 
     @Autowired
-    JwtAuthenticationService jwtAuthenticationService;
+    AuthenticationService authenticationService;
 
     @PostMapping("/authenticate")
     public ResponseEntity<JwtTokenResponse> createAuthenticationToken(@RequestBody JwtTokenRequest jwtTokenRequest){
-        return ResponseEntity.ok(jwtAuthenticationService.authenticate(jwtTokenRequest));
+        return ResponseEntity.ok(authenticationService.authenticate(jwtTokenRequest));
     }
 }
--- src/main/java/mateusz/michal/chat/Service/AuthenticationService.java
+++ src/main/java/mateusz/michal/chat/Service/AuthenticationService.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 
 @Service
-public class JwtAuthenticationService {
+public class AuthenticationService {
 
     @Autowired
     AuthenticationManager authenticationManager;
