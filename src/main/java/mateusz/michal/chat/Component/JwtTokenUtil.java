f815264c9d6e766687855a5ecb3bdfa528ee2eb4
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-12-21 17:25:32 +0100

    added method for decoding Jwt

--- src/main/java/mateusz/michal/chat/Component/JwtTokenUtil.java
+++ src/main/java/mateusz/michal/chat/Component/JwtTokenUtil.java
@@ -1,7 +1,10 @@
 package mateusz.michal.chat.Component;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +27,17 @@ public class JwtTokenUtil {
                 .withIssuedAt(new Date())
                 .sign(algorithm);
         return token;
+    }
+
+    public DecodedJWT decodeAndVerifyJwt(String token){
+        DecodedJWT decodedJWT = null;
+            try{
+                JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET_KEY))
+                .build();
+                decodedJWT = verifier.verify(token);
+                } catch (JWTVerificationException exception){
+                    exception.printStackTrace();
+                }
+                return decodedJWT;
     }
 }
