fe09b8490681996c044efe563cae9f7e99bd4146
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-12-22 16:32:53 +0100

    added UserProfilDTO model class

---
+++ src/main/java/mateusz/michal/chat/Model/UserProfilDTO.java
@@ -0,0 +1,13 @@
+package mateusz.michal.chat.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfilDTO {
+    String name;
+    String email;
+}
