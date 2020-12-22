3bd7a0c133718c26928d78910045dd8669ecdda5
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-12-22 16:24:15 +0100

    coded filtering requests

--- src/main/java/mateusz/michal/chat/Component/JwtFilter.java
+++ src/main/java/mateusz/michal/chat/Component/JwtFilter.java
@@ -1,10 +1,19 @@
 package mateusz.michal.chat.Component;
 
+import mateusz.michal.chat.Model.User;
+import mateusz.michal.chat.Repository.UserRepository;
+import mateusz.michal.chat.Service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,10 +21,40 @@ import java.io.IOException;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest,
-                                    HttpServletResponse httpServletResponse,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
 
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        Cookie tokenCookie = getTokenCookieFromCookies(request.getCookies());
+        if(tokenCookie != null) {
+            String username = jwtTokenUtil.getUsernameFromToken(tokenCookie.getValue());
+            UserDetails userDetails = myUserDetailsService.loadUserByUsername(username);
+            if (jwtTokenUtil.decodeAndVerifyJwt(tokenCookie.getValue(), userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private Cookie getTokenCookieFromCookies(Cookie[] cookies){
+        if (cookies == null){
+            return null;
+        }
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("token")){
+                return cookie;
+            }
+        }
+        return null;
     }
 }
