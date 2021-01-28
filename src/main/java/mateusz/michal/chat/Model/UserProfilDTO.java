699fdd2e37dbe802e288f94bd7f3bebfb3b27a66
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2021-01-28 12:30:26 +0100

    implemented IData interface to UserProfilDTO class

--- src/main/java/mateusz/michal/chat/Model/UserProfilDTO.java
+++ src/main/java/mateusz/michal/chat/Model/UserProfilDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserProfilDTO {
+public class UserProfilDTO implements IData {
     String name;
     String email;
 }
