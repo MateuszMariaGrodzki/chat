8841538f4bb0e93665b9ef664220910eb5e5ab85
Emzawadzki <emzawadzki@gmail.com>
2021-02-06 13:06:42 +0100

    feat: add rwd meta tag

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,6 +10,7 @@ const App = () => (
     <Helmet>
       <title>Chat App</title>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
     </Helmet>
     <CssBaseline />
     <UserContextProvider>
