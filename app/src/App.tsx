2c0e926151e82a8e6df250c22ffdd4d655d300f7
Emzawadzki <emzawadzki@gmail.com>
2021-02-06 14:12:07 +0100

    feat: allow multiple snackbars on register form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { CssBaseline } from "@material-ui/core";
+import { SnackbarProvider } from "notistack";
 
 import AppRouter from "@components/Router";
 import { UserContextProvider } from "@providers/UserProvider";
@@ -14,7 +15,9 @@ const App = () => (
     </Helmet>
     <CssBaseline />
     <UserContextProvider>
-      <AppRouter />
+      <SnackbarProvider variant="error">
+        <AppRouter />
+      </SnackbarProvider>
     </UserContextProvider>
   </>
 );
--- app/src/components/RegisterForm/RegisterForm.tsx
+++ app/src/components/RegisterForm/RegisterForm.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
-import { Button, Box, Snackbar } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
+import React from "react";
+import { Button, Box } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
+import RegisterAPI from "@api/RegisterAPI";
 import { Input } from "@common/Input";
 import { Form } from "@common/Form";
 import { useInput } from "@hooks/useInput";
 import { useStatus } from "@hooks/useStatus";
 import { paths } from "@config/paths";
-import RegisterAPI from "@api/RegisterAPI";
 
 const RegisterForm = () => {
-  const [name, handleUsernameChange] = useInput();
-  const [email, handleEmailChange] = useInput();
-  const [password, handlePasswordChange] = useInput();
   const [status, setStatus] = useStatus();
-  const [errors, setErrors] = useState<string[]>([]);
+  const resetFormStatus = () => {
+    setStatus(null);
+  };
 
+  const [name, handleUsernameChange] = useInput(resetFormStatus);
+  const [email, handleEmailChange] = useInput(resetFormStatus);
+  const [password, handlePasswordChange] = useInput(resetFormStatus);
   const history = useHistory();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     setStatus("pending");
@@ -29,12 +32,10 @@ const RegisterForm = () => {
       history.push(paths.registerSuccess);
     } else {
       setStatus("error");
-      setErrors(response.errors.map((error) => error.title));
+      response.errors.forEach((error) => {
+        enqueueSnackbar(error.title);
+      });
     }
-  };
-
-  const handleSnackbarClose = () => {
-    setStatus(null);
   };
 
   return (
@@ -44,16 +45,11 @@ const RegisterForm = () => {
         <Input placeholder="E-mail address" type="email" name="email" id="email" onChange={handleEmailChange} value={email} fullWidth />
         <Input placeholder="Password" type="password" name="pwd" id="pwd" onChange={handlePasswordChange} value={password} fullWidth />
         <Box mt={2} display="flex" justifyContent="center">
-          <Button variant="contained" size="large" type="submit" color="primary" disabled={status === "pending"}>
+          <Button variant="contained" size="large" type="submit" color="primary" disabled={status === "pending" || status === "error"}>
             Submit
           </Button>
         </Box>
       </Form>
-      {errors.map((error) => (
-        <Snackbar open={status === "error"} autoHideDuration={3000} onClose={handleSnackbarClose} key={error}>
-          <Alert severity="error">{error}</Alert>
-        </Snackbar>
-      ))}
     </>
   );
 };
