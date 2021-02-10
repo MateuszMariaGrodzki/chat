58e97557b1015adad284dd747c92222edd8f8757
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2021-02-10 12:01:54 +0100

    added 4 methods for creating response

--- server/src/main/java/mateusz/michal/chat/Structure/RespondStructure/JsonResponseFactory.java
+++ server/src/main/java/mateusz/michal/chat/Structure/RespondStructure/JsonResponseFactory.java
@@ -19,4 +19,20 @@ public class JsonResponseFactory {
         }
         throw new UnsupportedOperationException();
     }
+
+    public static IJsonResponse createResponse(List<MyError> errors, IMetaData metaData){
+        return new JsonErrorResponse(errors,metaData);
+    }
+
+    public static IJsonResponse createResponse(List<MyError> errors){
+        return new JsonErrorResponse(errors,null);
+    }
+
+    public static IJsonResponse createResponse(IData iData, IMetaData metaData){
+        return new JsonDataResponse(iData, metaData);
+    }
+
+    public static IJsonResponse createResponse(IData iData){
+        return new JsonDataResponse(iData, null);
+    }
 }
