4d5072efdce8faa966856f5d8774f53e7b87bfca
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2021-09-22 19:54:04 +0200

    added DatabaseLoaderErrorCode enum

---
+++ server/src/main/java/mateusz/michal/chat/DatabaseLoader/Model/DatabaseLoaderErrorCode.java
@@ -0,0 +1,9 @@
+package mateusz.michal.chat.DatabaseLoader.Model;
+
+import mateusz.michal.chat.Structure.RespondStructure.IMyEnum;
+
+public enum DatabaseLoaderErrorCode implements IMyEnum {
+    FILE_NOT_FOUND,
+    BAD_FILE_STRUCTURE
+
+}
